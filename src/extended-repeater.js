ae05fd6899e463184c2c0781513e503aa208819d
iKingsman <vladislavkingsman@gmail.com>
2025-01-05 20:53:58 +0300

    feat: add solutions for 5 functions

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -12,12 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * repeater('STRING', { repeatTimes: 3, separator: '**', 
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
- * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
+ * => 'STRING PLUS 00 PLUS 00P LUS ** STRING PLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const repeatTimes = options.repeatTimes || 1;
+  const separator = options.separator || '+';
+  const addition = options.addition !== undefined ? String(options.addition) : '';
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const additionSeparator = options.additionSeparator || '|';
+  const additionStr = new Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+  const finalStr = new Array(repeatTimes).fill(str + additionStr).join(separator);
+  return finalStr;
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const sortedArr = arr.filter(item => item !== -1).sort((a, b) => a - b);
+  const finalArr = [];
+  let index = 0;
+  arr.map(item => item === -1 ? finalArr.push(-1) : finalArr.push(sortedArr[index++]));
+  return finalArr;
 }
 
 module.exports = {
